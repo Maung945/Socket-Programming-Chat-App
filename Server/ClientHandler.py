bf5cd71aa902df69f7306b2e252b5087b5d275df
Maung945 <myo.aung8091@gmail.com>
2024-04-06 19:51:27 -0700

    ClientHandler.py bug fixed.

--- Server/ClientHandler.py
+++ Server/ClientHandler.py
@@ -11,7 +11,7 @@ from Common.Packet import TextPayload
 class ClientHandler():
     def __init__(self, active_clients_list):
         self.active_clients_list = active_clients_list
-        self.sent_messages_set = set()                  #Set to store previously sent messages...
+        self.sent_messages_set = set()                  # Set to store previously sent messages...
         
     def handle_client(self, client_socket):
         """
@@ -35,11 +35,11 @@ class ClientHandler():
                 content_str = client_socket.recv(2048).decode('utf-8')
                 if content_str:
                     message_obj = TextPayload.Generate(username_str, content_str)
-                    print(f'listen_for_messages(): {message_obj}')  #Debug line
+                    print(f'listen_for_messages(): {message_obj}')  # Debug line
                     self.send_messages_to_all(str(message_obj))
                 else:
                     print(f"The message sent from client {username_str} is empty...")
-                    break  #Client disconnected
+                    break  # Client disconnected
             except ConnectionResetError:
                 print(f"Client {username_str} disconnected")
                 self.cleanup_client(username_str, client_socket, f"{username_str} left the chatroom...")
@@ -58,17 +58,17 @@ class ClientHandler():
         """
         Send messages to all users in the connected users list...
         """
-        print(f'send_messages_to_all(): {message_str}')  #Debug line
+        print(f'send_messages_to_all(): {message_str}')  # Debug line
         if message_str not in self.sent_messages_set:
             disconnected_clients_list = []
             for username_str, client_socket in self.active_clients_list[:]:
                 try:
                     self.send_message_to_client(client_socket, message_str)
                 except BrokenPipeError: 
-                    #If a user is disconnected (broken pipe error), they will be removed from the list of active clients...
+                    # If a user is disconnected (broken pipe error), they will be removed from the list of active clients...
                     disconnected_clients_list.append((username_str, client_socket))
 
-            #Remove disconnected clients and update the active list...
+            # Remove disconnected clients and update the active list...
             for client_tuple in disconnected_clients_list:
                 self.active_clients_list.remove(client_tuple)
 
