70c726884b45956113ff7b4795e106c997850a34
SungeunAmberKim <sungeunkim@cpp.edu>
2024-05-05 19:41:58 -0700

    added kyber, needs to be edited more

--- Common/encryption_utils.py
+++ Common/encryption_utils.py
@@ -1,20 +1,28 @@
 from cryptography.fernet import Fernet
+import kyber
 import os
 
 
 def load_key():
     #dir_path = os.path.dirname(os.path.realpath(__file__))
-    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # Navigate up twice
-    key_path = os.path.join(dir_path, 'secret.key')
-    return open(key_path, "rb").read()
+    # dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # Navigate up twice
+    # key_path = os.path.join(dir_path, 'secret.key')
+    # return open(key_path, "rb").read()
     #key_path = os.path.join(dir_path, 'Common', 'secret.key')
     #return open(key_path, "rb").read()
     #return open("Common/secret.key", "rb").read()
+    pk, sk = kyber.Kyber512.keygen()
+    return pk, sk
 
-def encrypt_message(message, key):
-    f = Fernet(key)
-    return f.encrypt(message.encode())
+def encrypt_message(message, pk):
+    # f = Fernet(key)
+    # return f.encrypt(message.encode())
+    c, key = kyber.Kyber512.enc(pk)
+    return c, key
 
-def decrypt_message(encrypted_message, key):
-    f = Fernet(key)
-    return f.decrypt(encrypted_message).decode()
+
+def decrypt_message(c, sk):
+    # f = Fernet(key)
+    # return f.decrypt(encrypted_message).decode()
+    _key = kyber.Kyber512.dec(c, sk)
+    return _key
